de8e50619016960301da6c97f68522a233ebdfbb
user_ <91003748+rakesh5587@users.noreply.github.com>
2023-02-10 00:23:47 +0530

    Add files via upload

---
+++ Assignment 29th january.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1.	Guido van Rossum
+# 2.	Object-oriented programming and structured programming
+# 3.	Yes, Python is a case-sensitive language
+# 4.	.py
+# 5.	Python is an interpreted language
+# 6.	Indentation is used to define a block of code in python
+# 7.	#
+# 8.	Using python version () function, Using Python -V command.
+# 9.	Python supports to make anonymous functions which are not bound to a name, it uses a construct called "lambda”.
+# 10.	preferred installer program
+# 11.	print (), sum (), range ()
+# 12.	79 characters
+# 13.	Data science.
+#     Scientific and mathematical computing.
+#     Web development.
+#     Finance and trading.
+#     System automation and administration.
+#     Computer graphics.
+#     Basic game development.
+#     Security and penetration testing.
+# 14.	Memory management in Python involves a private heap containing all Python objects and data structures.
+# 15.	
+# 16.	Yes
+# 
+# 
